ac145d344acc99a5ee7693300da8e9ec30fd1cc1
Ahmet ALTUN <hi@ahmetaltun.dev>
2022-12-26 20:13:57 +0300

    config changes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'VueCountryDropdown2',
+      name: 'vue-country-dropdown-2',
       fileName: 'vue-country-dropdown-2',
       formats: ["umd", "es"]
     },
